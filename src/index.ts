8719961fe4e3c7f002bcc42610552077fa754105
OASISAsx <122677372+OASISAsx@users.noreply.github.com>
2025-12-20 22:53:59 +0700

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import app from './app'
+import app from "./app";
+import "dotenv/config";
 
-const PORT = 3000
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-})
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log("DATABASE_URL =", process.env.DATABASE_URL);
+});
